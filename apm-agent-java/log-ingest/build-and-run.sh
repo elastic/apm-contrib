040770efb0828f5ce4390515738ca2f0894d4989
Sylvain Juge <sylvain.juge@elastic.co>
2023-01-19 15:54:17 +0100

    cleanup volume on scenario startup

--- apm-agent-java/log-ingest/build-and-run.sh
+++ apm-agent-java/log-ingest/build-and-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 id=${1:-00}
 
+docker volume rm log-ingest_shared-volume-${id} 2>/dev/null
 docker-compose -f ./${id}-compose.yml build
 docker-compose -f ./${id}-compose.yml up
 docker-compose -f ./${id}-compose.yml down
