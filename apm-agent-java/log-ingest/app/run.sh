89314cbeb4ad7994057fb9b2cc7101b778bb754f
Sylvain Juge <sylvain.juge@elastic.co>
2023-01-19 15:26:22 +0100

    remove ability to use env file in app

--- apm-agent-java/log-ingest/app/run.sh
+++ apm-agent-java/log-ingest/app/run.sh
@@ -5,10 +5,6 @@ if [[ '' != "${ELASTIC_APM_SERVICE_NAME}" ]]; then
     javaagent_opt='-javaagent:/agent.jar'
 fi
 
-if [[ -f /env.sh ]]; then
-    source /env.sh
-fi
-
 cmd="java ${javaagent_opt:-} -jar /app.jar"
 echo "starting app with command: '${cmd}'"
 
